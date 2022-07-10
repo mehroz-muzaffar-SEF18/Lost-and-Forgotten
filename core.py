6a133896cc8de6cd7816e145e1fd4f1bcb114e9c
mehroz-muzaffar-SEF18 <bsef18m004@pucit.edu.pk>
2022-07-10 21:53:39 +0500

    Narrations Updated and Added Main Loop

--- core.py
+++ core.py
@@ -1,33 +1,85 @@
 import json
+import string
 from time import sleep
+
+from numpy import true_divide
+import re
 
 global settings
 settings={"Null":"Null"}
+global entities
+entities={"Null":"Null"}
+global narrations
+narrations={"Null":"Null"}
 
 def print_c(character, char_separator='', sync_print=True):
     print(character, end=char_separator, flush=sync_print)
 
+def load_json_data_from_file(path):
+    json_data_file = open(path)
+    json_data=json.load(json_data_file)
+    json_data_file.close()
+    return json_data
+
+def is_json_data_empty(data):
+    if "Null" in data.keys():
+        return True
+    return False
+
 def load_settings():
-    settings_file = open('settings.json')
     global settings
-    settings = json.load(settings_file)
-    settings_file.close()
+    settings = load_json_data_from_file("settings.json")
+
+def load_entities():
+    global entities
+    entities = load_json_data_from_file("entities.json")
+
+def load_narrations():
+    global narrations
+    narrations = load_json_data_from_file("narrations.json")
 
 def update_settings():
     settings_file = open('settings.json', 'w')
     settings_file.write(json.dumps(settings, indent=4))
     settings_file.close()
 
+def color_all_entities_in_narration(narration_text:str):
+    entities_array=[]
+    for category in entities.keys():
+        entities_array+=entities[category]
+    for entity in entities_array:
+        narration_text = re.sub(entity, color_entity_text(entity), narration_text, flags=re.IGNORECASE)
+        # narration_text=narration_text.replace(entity, color_entity_text(entity))
+    return narration_text
+
 def output_narration(narration_text):
     speed_map = {
-    'Low': 25,
-    'Medium': 50,
-    'High': 75
+        'Low': 25,
+        'Medium': 50,
+        'High': 75,
+        'Very High': 200
     }
     text_speed=50
     if "text_speed" in settings.keys():
         text_speed=speed_map[settings["text_speed"]]
     character_interval=1/text_speed
+    narration_text = color_all_entities_in_narration(narration_text)
     for ch in narration_text:
         sleep(character_interval)
         print_c(ch)
+
+def color_entity_text(item_name):
+    prefix = "\033["
+    reset = f"{prefix}0m"
+    color_category_map={
+        "enemies": "31m",#red
+        "places": "33m",#yellow
+        "interactables": "36m",#cyan
+        "pickables": "32m"#green
+    }
+    for category in entities.keys():
+        if item_name in entities[category]:
+            color_prefix = f"{prefix}{color_category_map[category]}"
+            return color_prefix+item_name+reset
+    return item_name
+
--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 import os
 import core
+from core import *
 
 
 os.system('color')
-prefix="\033["
-reset=f"{prefix}0m"
-red="31m"
+# prefix="\033["
+# reset=f"{prefix}0m"
+# red="31m"
 
-# arr=["Mehroz", "12"]
-str=f"My name is {prefix}{red}Mehroz{reset}. I am in class 12"
-str.replace("Mehroz", f"")
-print(str)
+# # arr=["Mehroz", "12"]
+# str=f"My name is {prefix}{red}Mehroz{reset}. I am in class 12"
+# str.replace("Mehroz", f"")
+
+load_settings()
+load_narrations()
+load_entities()
+
+game_end=False
+
+output_narration(core.narrations["begining"])
+
+while not game_end:
+    print_c("What would you like to do?\n")
+    input_words = input("Enter Command: ").split()
+
+    command = input_words[0]
+    is_single_word = False
+    if len(input_words)>=2:
+        entity = input_words[1].capitalize()
+    else:
+        is_single_word = True
+    if command in core.narrations.keys():
+        if is_single_word:
+            output_narration(core.narrations[command])
+        elif entity in core.narrations[command].keys():
+            output_narration(core.narrations[command][entity])
+        else:
+            output_narration(core.narrations["invalid_object"])
+    else:
+        output_narration(core.narrations["invalid_command"])
+    print()
+
 
 # str = "A man was found unconscious at the shore of an island far far away from the world."
 # core.load_settings()
